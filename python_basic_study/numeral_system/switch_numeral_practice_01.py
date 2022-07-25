57dcf6b2a932d2d82b1b92370eddbeb53623a124
Fola Flor <dpcalfola@gmail.com>
2022-07-25 21:26:58 +0900

    Study > (2022_07_25)

---
+++ python_basic_study/numeral_system/switch_numeral_practice_01.py
@@ -0,0 +1,9 @@
+print(5)
+
+print(bin(5))
+
+int_a = int(0b101)
+print(int_a)
+
+# 2진법 1010을 10진수로 만들기
+print(int('1010', 2))
---
+++ python_basic_study/python_practice/integer/integer_practice_01.py
@@ -0,0 +1,7 @@
+import math
+
+print(2 / 2)
+print(2 // 2)
+print(3 // 2)
+print(3 / 2)
+print(math.ceil(3 / 2))
